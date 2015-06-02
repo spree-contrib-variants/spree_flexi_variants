a9d2a9763263ba9ed95cd2c239a3db865938914a
Josua Schmid <josua.schmid@renuo.ch>
2015-06-02 09:43:19 +0200

    new hash syntax + quoting

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -1,9 +1,9 @@
-Deface::Override.new(:virtual_path => "spree/shared/_order_details",
-                     :name => "add_extra_order_details_line_item_description",
-                     :insert_bottom => "[data-hook='order_item_description']",
-                     :partial => "spree/shared/extra_order_details_line_item_description")
+Deface::Override.new(virtual_path: 'spree/shared/_order_details',
+                     name: 'add_extra_order_details_line_item_description',
+                     insert_bottom: '[data-hook="order_item_description"]',
+                     partial: 'spree/shared/extra_order_details_line_item_description')
 
-Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment_manifest",
-                     :name => "add_extra_order_details_line_item_description_admin",
-                     :insert_bottom => ".stock-item .item-name",
-                     :partial => "spree/shared/extra_order_details_line_item_description")
+Deface::Override.new(virtual_path: 'spree/admin/orders/_shipment_manifest',
+                     name: 'add_extra_order_details_line_item_description_admin',
+                     insert_bottom: '.stock-item .item-name',
+                     partial: 'spree/shared/extra_order_details_line_item_description')
