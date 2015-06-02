6d388bba52e02bc0f34c2839f863f01822b62f75
Josua Schmid <josua.schmid@renuo.ch>
2015-06-02 11:23:50 +0200

    show ad hoc options also on the delivery page

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -3,6 +3,11 @@ Deface::Override.new(virtual_path: 'spree/shared/_order_details',
                      insert_bottom: '[data-hook="order_item_description"]',
                      partial: 'spree/shared/extra_order_details_line_item_description')
 
+Deface::Override.new(virtual_path: 'spree/checkout/_shipment_items',
+                     name: 'add_extra_order_details_line_item_description',
+                     insert_bottom: '[data-hook="stock-contents"] .item-name',
+                     partial: 'spree/shared/extra_order_details_line_item_description')
+
 Deface::Override.new(virtual_path: 'spree/admin/orders/_shipment_manifest',
                      name: 'add_extra_order_details_line_item_description_admin',
                      insert_bottom: '.stock-item .item-name',
