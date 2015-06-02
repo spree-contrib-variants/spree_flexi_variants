4efa6c62699d1ba9d0fe4800a99002e98be32239
Josua Schmid <josua.schmid@renuo.ch>
2015-06-02 10:50:48 +0200

    additional partial for admin

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -6,4 +6,4 @@ Deface::Override.new(virtual_path: 'spree/shared/_order_details',
 Deface::Override.new(virtual_path: 'spree/admin/orders/_shipment_manifest',
                      name: 'add_extra_order_details_line_item_description_admin',
                      insert_bottom: '.stock-item .item-name',
-                     partial: 'spree/shared/extra_order_details_line_item_description')
+                     partial: 'spree/shared/extra_admin_order_details')
